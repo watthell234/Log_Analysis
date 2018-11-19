23ffbf3b8f9a3f3e11060cf75603f7a0ff083591
Julius Gonzalez <gonzalezjulius.c@gmail.com>
2018-11-19 01:06:10 -0500

    Fix style errors raised pycodestyles

--- reportdb.py
+++ reportdb.py
@@ -14,10 +14,14 @@ Join slug_views on aut.id = slug_views.author
 group by author_name order by views desc;"""
 
 ERROR_ONE = """select time::timestamp::date as date,
-100 * (sum(case when status = '404 NOT FOUND' then 1 else 0 end) / count(*)::real)
+100
+* (sum(case when status = '404 NOT FOUND' then 1 else 0 end) / count(*)::real)
 from log
 group by date
-having 100 * (sum(case when status = '404 NOT FOUND' then 1 else 0 end) / count(*)::real) > 1;"""
+having 100 *
+(sum(case when status = '404 NOT FOUND' then 1 else 0 end)
+ / count(*)::real) > 1;"""
+
 
 def get_reports(SQL):
     db = psycopg2.connect(dbname=DBNAME)
@@ -27,14 +31,28 @@ def get_reports(SQL):
     db.close()
     if SQL == TOP_THREE:
         with open("top_three_articles.txt", "w") as f:
-            f.writelines(str(r[0]) + " -- " + str("{:,}".format(r[1])) + " views" + "\n" for r in rows)
+            f.writelines(
+                        str(r[0]) + " -- " +
+                        str("{:,}".format(r[1])) +
+                        " views" +
+                        "\n" for r in rows)
 
     elif SQL == TOP_AUTHORS:
         with open("top_authors.txt", "w") as f:
-            f.writelines(str(r[0]) + " -- " + str("{:,}".format(r[1])) + " views" + "\n" for r in rows)
+            f.writelines(
+                        str(r[0]) + " -- " +
+                        str("{:,}".format(r[1])) +
+                        " views" +
+                        "\n" for r in rows
+                        )
     elif SQL == ERROR_ONE:
         with open("error_one.txt", "w") as f:
-            f.writelines(str(rows[0][0]) + " -- " + str("{0:.2f}%".format(rows[0][1])) + " errors")
+            f.writelines(
+                        str(rows[0][0]) +
+                        " -- " +
+                        str("{0:.2f}%".format(rows[0][1])) +
+                        " errors")
+
 
 get_reports(TOP_THREE)
 get_reports(TOP_AUTHORS)
