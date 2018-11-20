db2560c413e3e764ae03829bb240389ee11aeec0
vagrant <vagrant@vagrant.vm>
2018-11-20 05:13:16 +0000

    Refactor connection and report functions

--- reportdb.py
+++ reportdb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import psycopg2
-
+import time
 DBNAME = "news"
 
 TOP_THREE = """select title, views from slug_views
@@ -22,37 +22,58 @@ having 100 *
  / count(*)::real) > 1;"""
 
 
-def get_reports(SQL):
-    db = psycopg2.connect(dbname=DBNAME)
-    cur = db.cursor()
-    cur.execute(SQL)
-    rows = cur.fetchall()
+def connect(database_name):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        logger.warning("error creating database: %s", self.fmt_errmsg(e))
+
+
+
+def top_articles(sql):
+    db, c = connect(DBNAME)
+    c.execute(sql)
+    rows = c.fetchall()
     db.close()
-    if SQL == TOP_THREE:
-        with open("top_three_articles.txt", "w") as f:
-            f.writelines(
-                        str(r[0]) + " -- " +
-                        str("{:,}".format(r[1])) +
-                        " views" +
-                        "\n" for r in rows)
-
-    elif SQL == TOP_AUTHORS:
-        with open("top_authors.txt", "w") as f:
-            f.writelines(
-                        str(r[0]) + " -- " +
-                        str("{:,}".format(r[1])) +
-                        " views" +
-                        "\n" for r in rows
-                        )
-    elif SQL == ERROR_ONE:
-        with open("error_one.txt", "w") as f:
-            f.writelines(
-                        str(rows[0][0]) +
-                        " -- " +
-                        str("{0:.2f}%".format(rows[0][1])) +
-                        " errors")
+    with open("top_three_articles.txt", "w") as f:
+        f.writelines(
+                    str(r[0]) + " -- " +
+                    str("{:,}".format(r[1])) +
+                    " views" +
+                    "\n" for r in rows)
 
 
-get_reports(TOP_THREE)
-get_reports(TOP_AUTHORS)
-get_reports(ERROR_ONE)
+def top_authors(sql):
+    db, c = connect(DBNAME)
+    c.execute(sql)
+    rows = c.fetchall()
+    db.close()
+    with open("top_authors.txt", "w") as f:
+        f.writelines(
+                    str(r[0]) + " -- " +
+                    str("{:,}".format(r[1])) +
+                    " views" +
+                    "\n" for r in rows
+                    )
+
+
+def error_one(sql):
+    db, c = connect(DBNAME)
+    c.execute(sql)
+    rows = c.fetchall()
+    db.close()
+    with open("error_one.txt", "w") as f:
+        f.writelines(
+                    str(rows[0][0]) +
+                    " -- " +
+                    str("{0:.2f}%".format(rows[0][1])) +
+                    " errors")
+
+
+if __name__ == '__main__':
+    top_articles(TOP_THREE)
+    top_authors(TOP_AUTHORS)
+    error_one(ERROR_ONE)
